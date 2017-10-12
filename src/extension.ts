e7da806911c7b00b3efea87187e97d649ebb7884
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-10-12 11:12:24 +0300

    rename

--- src/extension.ts
+++ src/extension.ts
@@ -32,7 +32,7 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.evaluateSelection', () => {
+    let disposable = vscode.commands.registerCommand('extension.evaluateSelectionInplace', () => {
         const editor = vscode.window.activeTextEditor;
         var selections: vscode.Selection[] = editor.selections;
         selections = selections.sort(compareSelection)
